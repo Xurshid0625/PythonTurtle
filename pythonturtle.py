029176fe53beb851e630dd5591c055fc17acdb59
Xurshid Abdumannonov <abdumannonovxurshid0625@gmail.com>
2026-01-19 15:43:38 +0500

    create square by turtle

--- pythonturtle.py
+++ pythonturtle.py
@@ -2,5 +2,20 @@ from turtle import Turtle, Screen
 
 
 window = Screen()
+window.title("Mening Oynam")
+
+line = Turtle()
+line.color("Blue")
+line.pensize(5)
+line.speed(0)
+line.up()
+line.goto(300, 300)
+line.down()
+line.goto(300, -300)
+line.goto(-300, -300)
+line.goto(-300, 300)
+line.goto(300, 300)
+line.hideturtle()
+
 
 window.mainloop()
