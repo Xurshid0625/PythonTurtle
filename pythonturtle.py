5d7d353f76de507a6bf8ee625b8a02c12798b1db
Xurshid Abdumannonov <abdumannonovxurshid0625@gmail.com>
2026-01-19 15:58:06 +0500

    ball step around square

--- pythonturtle.py
+++ pythonturtle.py
@@ -1,6 +1,5 @@
 from turtle import Turtle, Screen
 
-
 window = Screen()
 window.title("Mening Oynam")
 
@@ -16,6 +15,26 @@ line.goto(-300, -300)
 line.goto(-300, 300)
 line.goto(300, 300)
 line.hideturtle()
+
+ball = Turtle()
+ball.color("Yellow")
+ball.shape("circle")
+ball.up()
+ball.goto(0, 0)
+
+step_x = 3
+step_y = 2
+
+while True:
+
+    x, y = ball.position()
+
+    if x + step_x >= 300 or x + step_x <= -300:
+        step_x = -step_y
+    if y + step_y >= 300 or y + step_y <= -300:
+        step_y = -step_y
+
+    ball.goto(x + step_x, y + step_y)
 
 
 window.mainloop()
